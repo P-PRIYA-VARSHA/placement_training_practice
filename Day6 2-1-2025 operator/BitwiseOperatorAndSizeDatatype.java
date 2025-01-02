c993a67fb735e3074c31d52a7ee225354445dfc2
PRIYA VARSHA <priyavarsha.parthipan@gmail.com>
2025-01-02 12:16:26 +0530

    bitwise  operator and size of datatype using SIZE

---
+++ Day6 2-1-2025 operator/BitwiseOperatorAndSizeDatatype.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class BitwiseOperatorAndSizeDatatype
+{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+System.out.println(a&b);
+System.out.println(a|b);
+System.out.println(Integer.SIZE);
+System.out.println(Character.SIZE);
+System.out.println(a<<2);
+System.out.println(a<<1);
+System.out.println(~a);
+System.out.println(~b);
+
+
+
+
+
+
+
+}
+
+}

af7e9efd566dc1faef4ead6437fa9963236a0a98
PRIYA VARSHA <priyavarsha.parthipan@gmail.com>
2025-01-02 11:23:07 +0530

    Asssignment operator

---
+++ Day6 2-1-2025 operator/Assign.java
@@ -0,0 +1,16 @@
+class Assign{
+public static void main(String args[]){
+
+int c=6;
+
+System.out.println("The value is: "+(c+=7));
+System.out.println("The value is: "+(c-=7));
+System.out.println("The value is: "+(c*=7));
+System.out.println("The value is: "+(c/=7));
+
+
+
+
+}
+
+}

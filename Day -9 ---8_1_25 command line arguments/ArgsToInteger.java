f2e7f922ac5240f222b8189ac8148e7ad158942e
PRIYA VARSHA <priyavarsha.parthipan@gmail.com>
2025-01-19 19:14:09 +0530

    some rename of folders and command line argument

---
+++ Day -9 ---8_1_25 command line arguments/ArgsToInteger.java
@@ -0,0 +1,21 @@
+public class ArgsToInteger{
+public static void main(String args[]){
+
+if(args.length!=1){
+System.out.println("Enter a single command Line argument");
+return;
+}
+else{try{
+int number = Integer.parseInt(args[0]);
+
+int sq=number*number;
+System.out.println("The square of number is: "+sq);
+
+}catch(NumberFormatException e){
+System.out.println("Enter a valid input");
+}
+}
+
+
+}
+}
---
+++ Day -9 ---8_1_25 command line arguments/NumberOfCommandLineArgs.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class NumberOfCommandLineArgs{
+public static void main(String args[]){
+int count=args.length;
+System.out.println("The number of arguments passed are: "+count);
+}
+}

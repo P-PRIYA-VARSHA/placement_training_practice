cb503ee4398a1074dd49c39d93680e81b3ea6f85
PRIYA VARSHA <priyavarsha.parthipan@gmail.com>
2025-01-20 10:02:07 +0530

    sum of Recursion numbers

---
+++ Day 11 --- 20-1-2025 Recursion/SumRecursion.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class SumRecursion{
+static int sum(int n){
+if(n==0)
+return 0;
+else
+return n+sum(n-1);
+}
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+System.out.println("The sum of recursion of "+n+"is: "+sum(n));
+}
+}
+
+
+
